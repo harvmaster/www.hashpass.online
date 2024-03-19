aaedb480818d408b5263176235dcdea6c9a19974
Harvey Zuccon <hzuccon@gmail.com>
2024-03-19 18:09:43 +1100

    Updated api endpoint

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,8 @@ export default {
   created: function() {
     // Setup Base URL for API
     //axios.defaults.baseURL = 'http://localhost:3000/api'
-    axios.defaults.baseURL = 'https://api.hashpass.online/api';
+    // axios.defaults.baseURL = 'https://api.hashpass.online/api';
+    axios.defaults.baseURL = 'https://sdbapi.hzuccon.com/api';
 
     // If there is a JWT token, get the user
     var jwtToken = Cookies.get('jwt_token');
